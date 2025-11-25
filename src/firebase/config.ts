0ec0455d6df750f411482a9a7690b11ae17cb40b
Liam Coffey <liamcoffey24@gmail.com>
2025-11-25 07:06:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1011487900-9ef8e",
+  "appId": "1:321665756190:web:47c1b7cfc670707cd2d20f",
+  "apiKey": "AIzaSyCUExG5hMeRaCYhl5ol1Hhn3Zxhf1yTRqY",
+  "authDomain": "studio-1011487900-9ef8e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "321665756190"
+};
